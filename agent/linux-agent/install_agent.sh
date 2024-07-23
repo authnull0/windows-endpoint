32e0ea3c740db367273fe9e18e27b7dc59941edf
manikauth <manikandan@authnull.com>
2024-07-23 22:34:40 +0530

    fix:modfied the linux_agent.sh file

--- agent/linux-agent/install_agent.sh
+++ agent/linux-agent/install_agent.sh
@@ -3,19 +3,12 @@
 # Download the agent file
 sudo wget https://github.com/authnull0/windows-endpoint/raw/linux-agent/agent/linux-agent/agentv2 -O agentv2
 
-
-
 # Prompt for app.env file content
 echo "Please enter the content for the app.env file. End with an empty line or Ctrl+D:"
-
-# Create the app.env file and read the input directly into it
-cat app.env
-
+cat > app.env
 
 # Make the agent file executable
 sudo chmod +x agentv2
-
-
 
 # Run the agent
 ./agentv2
