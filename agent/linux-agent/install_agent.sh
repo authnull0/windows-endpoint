eb80b0e0f70491518852df176bb979c5716a04c1
manikauth <manikandan@authnull.com>
2024-07-24 10:23:08 +0530

    modified the script

--- agent/linux-agent/install_agent.sh
+++ agent/linux-agent/install_agent.sh
@@ -6,11 +6,17 @@ sudo wget https://github.com/authnull0/windows-endpoint/raw/linux-agent/agent/li
 echo "Please enter the content for the app.env file. End with an empty line or Ctrl+D:"
 
 
-# Read multiple lines of input
+# Initialize an empty string to store the content
 app_env_content=""
+
+# Read input line by line
 while IFS= read -r line || [ -n "$line" ]; do
+    # Append the line and a newline character to app_env_content
     app_env_content+="$line"$'\n'
 done
+
+# Remove the trailing newline character
+app_env_content=${app_env_content%$'\n'}
 
 # Create the app.env file with the provided content
 echo -n "$app_env_content" > app.env
