85ec7ea76d28b99d8e2206e373431f5cf98dd76d
safeya11 <safiya@authnull.com>
2025-01-17 16:36:49 +0530

    script fix to avoid writing duplicate entries

--- pam/google-authenticator-pam.sh
+++ pam/google-authenticator-pam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download Files
-sudo wget -P /tmp https://github.com/authnull0/windows-endpoint/raw/google-authenticator-pam/pam/pam_google_authenticator.so
+#sudo wget -P /tmp https://github.com/authnull0/windows-endpoint/raw/google-authenticator-pam/pam/pam_google_authenticator.so
 sudo wget -P /tmp https://github.com/authnull0/windows-endpoint/raw/google-authenticator-pam/pam/did.sh
 sudo wget -P /tmp https://github.com/authnull0/windows-endpoint/raw/google-authenticator-pam/pam/log_pam_rhost.sh
 
@@ -10,7 +10,7 @@ echo "Files downloaded. Check the logs with: tail -f /var/log/auth.log (Ubuntu) 
 
 # Move Files
 sudo mkdir -p /usr/local/lib/security
-sudo mv /tmp/pam_google_authenticator.so /usr/local/lib/security
+#sudo mv /tmp/pam_google_authenticator.so /usr/local/lib/security
 sudo mv /tmp/did.sh /
 sudo mv /tmp/log_pam_rhost.sh /usr/local/bin/
 sudo chmod +x /did.sh
@@ -44,9 +44,10 @@ if ! grep -q "AuthenticationMethods keyboard-interactive" /etc/ssh/sshd_config; 
     sudo sh -c 'echo "AuthenticationMethods keyboard-interactive" >> /etc/ssh/sshd_config'
 fi
 
-if grep -q "KbdInteractiveAuthentication no" /etc/ssh/sshd_config; then
-    sudo sed -i 's/KbdInteractiveAuthentication no/KbdInteractiveAuthentication yes/g' /etc/ssh/sshd_config'
+if grep -q "^KbdInteractiveAuthentication no" /etc/ssh/sshd_config; then
+    sudo sed -i 's/^KbdInteractiveAuthentication no/KbdInteractiveAuthentication yes/g' /etc/ssh/sshd_config
 fi
+
 # Log the SSHD config status
 echo "SSHD config configured. Check the logs with: tail -f /var/log/auth.log (Ubuntu) or tail -f /var/log/secure (CentOS)"
 
